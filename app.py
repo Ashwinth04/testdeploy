30c7520c187934bbc9c7c657478313615694b7c6
Ashwinth Anbu <ashwinth04@gmail.com>
2024-07-31 19:55:43 +0530

    Added groq llm

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from langchain.prompts import PromptTemplate
-from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
 from langchain.chains import LLMChain
 import streamlit as st
 
-llm = Ollama(model="qwen2:1.5b")
+groq_api_key = "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"
+llm = ChatGroq(groq_api_key = groq_api_key,model="mixtral-8x7b-32768")
 
 text_generation_prompt = PromptTemplate(
     template="Generate some text about the following topic: {topic}",
